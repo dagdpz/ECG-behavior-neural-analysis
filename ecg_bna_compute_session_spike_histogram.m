2000e5030df4278db6c115207336c20a7681796d
lubavas <19555010+lubavas@users.noreply.github.com>
2023-10-30 17:45:37 +0100

    clear up

--- ecg_bna_compute_session_spike_histogram.m
+++ ecg_bna_compute_session_spike_histogram.m
@@ -1,5 +1,4 @@
 function Output=ecg_bna_compute_session_spike_histogram(session_info,Rpeaks,ecg_bna_cfg,trials)
-savePlot = 1;
 Sanity_check=0; % ECG triggered ECG, turn off since typically there is no ECG data in the spike format
 
 basepath_to_save=[session_info.SPK_fldr filesep 'per_unit'];
@@ -13,46 +12,6 @@ condition_labels={'Rest','Task'};
 % Rpeaks derived from concatenated ECG data [First_trial_INI.ECG1 trial.TDT_ECG1]
 %load(session_info.Input_ECG);
 load(session_info.Input_spikes);
-
-% offset_blocks_Rpeak=0;
-% for b=1:numel(out)
-%     Rpeaks(b).block=out(b).nrblock_combinedFiles;
-%     if isempty(out(b).nrblock_combinedFiles) || isempty(out(b).Rpeak_t(1)) || isempty(out(b).R2R_t)
-%         Rpeaks(b).block=NaN;
-%     end
-%     RPEAK_ts=[out(b).Rpeak_t(1) intersect(out(b).Rpeak_t,out(b).R2R_t)];
-%     RPEAKS_intervals=diff(RPEAK_ts);
-%     ecg_R2Rt_mean=mean(RPEAKS_intervals);
-%     idx_valid = RPEAKS_intervals<1.5*ecg_R2Rt_mean; %use mode or mean ?
-%     nonval_idx=find([0, ~idx_valid]);
-%     invalid_intervals=[NaN,NaN];
-%     for iv=1:numel(nonval_idx)
-%         invalid_intervals(iv,1)=RPEAK_ts(nonval_idx(iv)-1);
-%         invalid_intervals(iv,2)=RPEAK_ts(nonval_idx(iv));
-%     end
-%     RPEAKS_intervals=RPEAKS_intervals(idx_valid);
-%     ecg_R2Rt_std=std(RPEAKS_intervals);
-%     N=ecg_bna_cfg.n_permutations;
-%     [~,ix]=sort(rand(N,length(RPEAKS_intervals)),2);
-%     perm=RPEAKS_intervals(ix);
-%     
-%     %% here we jitter all Rpeak intervals
-%     RPEAKS_intervals_p = [repmat(RPEAK_ts(1),N,1) repmat(RPEAKS_intervals,N,1)+randn(N,length(RPEAKS_intervals))*ecg_R2Rt_std perm]; % jittering every interval!
-%     RPEAK_ts_p=cumsum(RPEAKS_intervals_p,2);
-%     
-%     %% re-evaluating valid intervals... this is important to fix surrogates being higher due to periods of increased spiking that correlate with invalid Rpeaks
-%     for iv=1:size(invalid_intervals,1)
-%         RPEAK_ts_p(RPEAK_ts_p>invalid_intervals(iv,1)+ecg_R2Rt_mean/2 & RPEAK_ts_p<invalid_intervals(iv,2)-ecg_R2Rt_mean/2)=NaN;
-%     end
-%     RPEAK_ts_p(RPEAK_ts_p>max(RPEAK_ts)+max(RPEAKS_intervals))=NaN;
-%     RPEAK_ts_p(:,all(isnan(RPEAK_ts_p),1))=[];
-%     Rpeaks(b).RPEAK_ts=RPEAK_ts+offset_blocks_Rpeak(b);
-%     Rpeaks(b).shuffled_ts=RPEAK_ts_p+offset_blocks_Rpeak(b);
-%     Rpeaks(b).offset=offset_blocks_Rpeak(b);
-%     offset_blocks_Rpeak(b+1)=offset_blocks_Rpeak(b)+max(RPEAK_ts)+max(RPEAKS_intervals)*2;
-% end
-% offset_blocks_Rpeak(end)=[];
-% Rblocks=[Rpeaks.block];
 
 offset_blocks_Rpeak=[Rpeaks.offset];
 Rblocks=[Rpeaks.block];
