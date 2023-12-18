18cc3f9b9c22ba01e131d71a6991fcdf7b289f15
lubavas <19555010+lubavas@users.noreply.github.com>
2023-12-18 16:38:46 +0100

    update copying function

--- ecg_bna_copy_selected_units.m
+++ ecg_bna_copy_selected_units.m
@@ -1,54 +1,28 @@
-function ecg_bna_copy_selected_units(cfg)
+function ecg_bna_copy_selected_units(unit_list, destination_folder, cfg)
 
-exclusion_lists = dir([cfg.SPK_root_results_fldr filesep '*unitInfo*.mat']);
+load([cfg.unit_lists filesep unit_list], 'unit_ids');
 
-from_folder = [cfg.SPK_root_results_fldr filesep 'per_unit\'];
+if ~exist(destination_folder, 'dir')
+    mkdir(destination_folder)
+end
 
-%%
-for listNum = 1:length(exclusion_lists)
+for unitNum = 1:length(unit_ids)
     
-    unit_list = load([exclusion_lists(listNum).folder filesep exclusion_lists(listNum).name]);
+    fileList = dir([cfg.per_session_folder filesep '*' unit_ids{unitNum} '*']);
     
-    fns = fieldnames(unit_list);
+    if isempty(fileList)
+        disp(['No files found for ' unit_ids{unitNum}])
+    end
     
-    if contains(exclusion_lists(listNum).name, 'stableTaskAndRest')
-        if contains(exclusion_lists(listNum).name, '_600')
-            to_folder = [from_folder(1:end-1) '_selected_stableTaskAndRest_600'];
-        elseif contains(exclusion_lists(listNum).name, 'after_exclusion')
-            to_folder = [from_folder(1:end-1) '_selected_stableTaskAndRest'];
+    for flNum = 1:length(fileList)
+        
+        [success,message] = ...
+            copyfile([fileList(flNum).folder filesep fileList(flNum).name], destination_folder);
+        if success
+            disp(['Success: ' fileList(flNum).name '  --->  ' destination_folder])
+            fprintf('\n')
         else
-            to_folder = [from_folder(1:end-1) '_excluded_stableTaskAndRest'];
-        end
-    elseif contains(exclusion_lists(listNum).name, 'after_exclusion')
-        to_folder = [from_folder(1:end-1) '_selected'];
-    elseif contains(exclusion_lists(listNum).name, 'excluded')
-        to_folder = [from_folder(1:end-1) '_excluded'];
-    elseif contains(exclusion_lists(listNum).name, 'before_exclusion')
-        continue
-    end
-    
-    if ~exist(to_folder, 'dir')
-        mkdir(to_folder)
-    end
-    
-    for unitNum = 1:length(unit_list.(fns{1}))
-        
-        fileList = dir([from_folder '*' unit_list.(fns{1}){unitNum} '*']);
-        
-        if isempty(fileList)
-            disp(['No files found for ' unit_list.(fns{1}){unitNum}])
-        end
-        
-        for flNum = 1:length(fileList)
-        
-            [success,message] = ...
-                copyfile([fileList(flNum).folder filesep fileList(flNum).name], to_folder);
-            if success
-                disp(['Success: ' fileList(flNum).name '  --->  ' to_folder])
-                fprintf('\n')
-            else
-                disp('Copying failed ...')
-            end
+            disp('Copying failed ...')
         end
     end
 end
