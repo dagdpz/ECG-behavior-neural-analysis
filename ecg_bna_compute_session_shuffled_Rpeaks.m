bf0b47075b0265a3f5ff2808ca2fa9b77e059187
lubavas <19555010+lubavas@users.noreply.github.com>
2024-02-12 15:18:08 +0100

    take inhalation and exhalation into account (the basic R-peak reshuffling is unchanged)

--- ecg_bna_compute_session_shuffled_Rpeaks.m
+++ ecg_bna_compute_session_shuffled_Rpeaks.m
@@ -3,6 +3,7 @@ function Rpeaks=ecg_bna_compute_session_shuffled_Rpeaks(session_info,cfg)
 load(session_info.Input_ECG);
 
 % get the number of permutations from settings
+global N
 N=cfg.n_permutations;
 
 % % condition for using CAP data instead of ECG 
@@ -34,56 +35,76 @@ for b=1:numel(out)
         continue
     end
     
-    RPEAK_ts=out(b).Rpeak_t; 
-	RPEAKS_intervals=diff(RPEAK_ts);                               % These are the intervals of our valid Rpeaks, the ones we are going to jitter
+    [RPEAK_ts, RPEAK_dur, RPEAK_ts_p, RPEAK_dur_p, allowed_jitter_range] = jittering_core_function(out(b).Rpeak_t, out(b).R2R_valid, out(b).R2R_t, out(b).idx_valid_R2R_consec);
     
-	%% shuffling the VALID R2R intervals to append at the end of our jittered intervals, 
-    %  just in case we randomly end up not covering the entire time window
-    %  Don't worry, most (typically all) of it is going to be removed 
-    [~,ix]=sort(rand(N,length(out(b).R2R_valid)),2);
-    perm=out(b).R2R_valid(ix);
-    
-	%% here we jitter all Rpeak intervals
-	allowed_jitter_range=std(out(b).R2R_valid);                    % one-sided std of normally distributed jitter values
-    RPEAKS_intervals_p = [repmat(RPEAK_ts(1),N,1) repmat(RPEAKS_intervals,N,1)+randn(N,length(RPEAKS_intervals))*allowed_jitter_range perm]; % jittering every interval!
-    RPEAK_ts_p         = cumsum(RPEAKS_intervals_p,2);
-    RPEAK_ts_dur       = [zeros(N,1) diff(RPEAK_ts_p,1,2)];
-    
-    %% figure out consecutive RR-intervals - we do it only after jittering because we need to know durations of jittered intervals corresponding to the consecutive ones
-    [~,consecutive_idx]=ismember(out(b).R2R_t(out(b).idx_valid_R2R_consec),out(b).Rpeak_t); % indexing of consecutive_idx corrsponds to out(b).R2R_t
-    valid_idx=consecutive_idx-1;
-    next_invalid=diff(valid_idx)~=1;                               % is the next Rpeak invalid (i.e. followed by invalid R2R interval)
-    iv_starts  =[0  RPEAK_ts(valid_idx([next_invalid true]))];     % start of invalid intervals: Timestamps of valid Rpeaks followed by invalid ones
-                                                                   % First Segment (for 0 to first valid Rpeak) and last segment
-                                                                   % (everything after last valid Rpeak) are always invalid
-    iv_ends    =[RPEAK_ts(valid_idx([true next_invalid]))   inf];  % end of invalid intervals: Timestamps of valid Rpeaks PRECEDED by invalid ones  
-                                                                   % we can get Rpeak-ts preceded by invalid R2R by shifting next_invalid  
-    grace_window=mean(out(b).R2R_valid)/2;                         % +/- Range for shuffled Rpeaks to be allowed inside invalid segments
-    
-    %% take data corresponding to consecutive R-peaks
-    RPEAK_ts     = RPEAK_ts(valid_idx);                                  % take only Rpeaks surrounded by valid R2R
-    RPEAK_dur    = out(b).R2R_valid(out(b).idx_valid_R2R_consec-1);      % 
-    
-    %% remove jittered Rpeaks and corresponding durations that fell into invalid segments
-    for iv=1:numel(iv_starts)
-        idx2exclude_1 = RPEAK_ts_p>iv_starts(iv)+grace_window & RPEAK_ts_p<iv_ends(iv)-grace_window;
-        RPEAK_ts_p   (idx2exclude_1) = 0;
-        RPEAK_ts_dur (idx2exclude_1) = NaN;
-    end
-    idx2exclude_2 = RPEAK_ts_p>max(RPEAK_ts)+allowed_jitter_range;
-    RPEAK_ts_p    (idx2exclude_2) = 0;
-    RPEAK_ts_dur  (idx2exclude_2) = NaN;
-    
-    RPEAK_ts_p   (:,all(RPEAK_ts_p==0,1)) = [];
-    RPEAK_ts_dur (:,all(isnan(RPEAK_ts_dur),1)) = [];
-    
+    %% put the data together
     Rpeaks(b).RPEAK_ts=RPEAK_ts+offset_blocks_Rpeak(b);         % this offset is just a trick to be able to append Rpeaks across blocks easily
     Rpeaks(b).RPEAK_dur=RPEAK_dur; % durations of RR-intervals (the corresponding ends of those intervals are in Rpeaks(b).RPEAK_ts)
     Rpeaks(b).shuffled_ts=RPEAK_ts_p+offset_blocks_Rpeak(b);
     Rpeaks(b).shuffled_ts(Rpeaks(b).shuffled_ts==offset_blocks_Rpeak(b))=0;
     
-    Rpeaks(b).shuffled_dur = RPEAK_ts_dur; % durations of reshuffled RR-intervals (the corresponding ends of those intervals are in Rpeaks(b).shuffled_ts)
+    Rpeaks(b).shuffled_dur = RPEAK_dur_p; % durations of reshuffled RR-intervals (the corresponding ends of those intervals are in Rpeaks(b).shuffled_ts)
     offset_blocks_Rpeak(b+1)=offset_blocks_Rpeak(b)+max(RPEAK_ts)+allowed_jitter_range*2;
+    
+    clear RPEAK_ts RPEAK_dur RPEAK_ts_p RPEAK_dur_p allowed_jitter_range
+    
+    %% jitter data for exhalation and inhalation
+    if isfield(out, 'is_RR_insp') && sum(out(b).is_RR_insp)>1 && sum(out(b).is_RR_exp)>1
+        % inhalation first
+        RPEAK_ts             = out(b).Rpeak_t(out(b).is_R_peak_insp);
+        R2R_valid            = out(b).R2R_valid(out(b).is_RR_insp);
+        R2R_t                = out(b).R2R_t(out(b).is_RR_insp);
+        
+        % prepare idx_valid_R2R_consec
+        R2R_consec     = out(b).R2R_t(out(b).idx_valid_R2R_consec);
+        R2R_intersect  = intersect(R2R_consec,R2R_t);
+        idx_valid_R2R_consec = find(ismember(R2R_intersect, R2R_t));
+        if idx_valid_R2R_consec(1) == 1
+            % drop the first Rpeak if it's consecutive as the jittering 
+            % function complains about such a situation
+            idx_valid_R2R_consec(1) = [];
+        end
+        
+        [RPEAK_ts, RPEAK_dur, RPEAK_ts_p, RPEAK_dur_p] = ...
+            jittering_core_function(RPEAK_ts, R2R_valid, R2R_t, idx_valid_R2R_consec);
+        
+        Rpeaks(b).RPEAK_ts_insp=RPEAK_ts+offset_blocks_Rpeak(b);         % this offset is just a trick to be able to append Rpeaks across blocks easily
+        Rpeaks(b).RPEAK_dur_insp=RPEAK_dur; % durations of RR-intervals (the corresponding ends of those intervals are in Rpeaks(b).RPEAK_ts)
+        Rpeaks(b).shuffled_ts_insp=RPEAK_ts_p+offset_blocks_Rpeak(b);
+        Rpeaks(b).shuffled_ts_insp(Rpeaks(b).shuffled_ts_insp==offset_blocks_Rpeak(b))=0;
+    
+        Rpeaks(b).shuffled_dur_insp = RPEAK_dur_p; % durations of reshuffled RR-intervals (the corresponding ends of those intervals are in Rpeaks(b).shuffled_ts)
+        
+        clear RPEAK_ts RPEAK_dur RPEAK_ts_p RPEAK_dur_p allowed_jitter_range
+        
+        % exhalation second
+        RPEAK_ts             = out(b).Rpeak_t(out(b).is_R_peak_exp);
+        R2R_valid            = out(b).R2R_valid(out(b).is_RR_exp);
+        R2R_t                = out(b).R2R_t(out(b).is_RR_exp);
+        
+        % prepare idx_valid_R2R_consec
+        R2R_consec     = out(b).R2R_t(out(b).idx_valid_R2R_consec);
+        R2R_intersect  = intersect(R2R_consec,R2R_t);
+        idx_valid_R2R_consec = find(ismember(R2R_intersect, R2R_t));
+        if idx_valid_R2R_consec(1) == 1
+            % drop the first Rpeak if it's consecutive as the jittering 
+            % function complains about such a situation
+            idx_valid_R2R_consec(1) = [];
+        end
+        
+        [RPEAK_ts, RPEAK_dur, RPEAK_ts_p, RPEAK_dur_p] = ...
+            jittering_core_function(RPEAK_ts, R2R_valid, R2R_t, idx_valid_R2R_consec);
+        
+        Rpeaks(b).RPEAK_ts_exp=RPEAK_ts+offset_blocks_Rpeak(b);         % this offset is just a trick to be able to append Rpeaks across blocks easily
+        Rpeaks(b).RPEAK_dur_exp=RPEAK_dur; % durations of RR-intervals (the corresponding ends of those intervals are in Rpeaks(b).RPEAK_ts)
+        Rpeaks(b).shuffled_ts_exp=RPEAK_ts_p+offset_blocks_Rpeak(b);
+        Rpeaks(b).shuffled_ts_exp(Rpeaks(b).shuffled_ts_exp==offset_blocks_Rpeak(b))=0;
+    
+        Rpeaks(b).shuffled_dur_exp = RPEAK_dur_p; % durations of reshuffled RR-intervals (the corresponding ends of those intervals are in Rpeaks(b).shuffled_ts)
+        
+        clear RPEAK_ts RPEAK_dur RPEAK_ts_p RPEAK_dur_p allowed_jitter_range
+        
+    end
     
     %% This figure plots all R-peaks and consecutive R-peaks to make sure our selection procedure does the right thing
 %     figure
@@ -100,5 +121,52 @@ for b=1:numel(out)
 %     legend([f st1 st2 st3], {'Invalid Time Window', 'All R-peaks', 'Ends of valid RRs', 'Consecutive R-peaks: 1 valid RR before & 1 valid RR after'})
 %     xlabel('Time, s')
 end
+
+end
+
+function [RPEAK_ts, RPEAK_dur, RPEAK_ts_p, RPEAK_dur_p, allowed_jitter_range] = jittering_core_function(RPEAK_ts, R2R_valid, R2R_t, idx_valid_R2R_consec)
+global N
+% arguments: Rpeak_t, R2R_valid, R2R_t, idx_valid_R2R_consec
+RPEAKS_intervals=diff(RPEAK_ts);                               % These are the intervals of our valid Rpeaks, the ones we are going to jitter
+
+%% shuffling the VALID R2R intervals to append at the end of our jittered intervals,
+%  just in case we randomly end up not covering the entire time window
+%  Don't worry, most (typically all) of it is going to be removed
+[~,ix]=sort(rand(N,length(R2R_valid)),2);
+perm=R2R_valid(ix);
+
+%% here we jitter all Rpeak intervals
+allowed_jitter_range = std(R2R_valid);                    % one-sided std of normally distributed jitter values
+RPEAKS_intervals_p   = [repmat(RPEAK_ts(1),N,1) repmat(RPEAKS_intervals,N,1)+randn(N,length(RPEAKS_intervals))*allowed_jitter_range perm]; % jittering every interval!
+RPEAK_ts_p           = cumsum(RPEAKS_intervals_p,2);
+RPEAK_dur_p          = [zeros(N,1) diff(RPEAK_ts_p,1,2)];
+
+%% figure out consecutive RR-intervals - we do it only after jittering because we need to know durations of jittered intervals corresponding to the consecutive ones
+[~,consecutive_idx]=ismember(R2R_t(idx_valid_R2R_consec),RPEAK_ts); % indexing of consecutive_idx corrsponds to out(b).R2R_t
+valid_idx=consecutive_idx-1;
+next_invalid=diff(valid_idx)~=1;                               % is the next Rpeak invalid (i.e. followed by invalid R2R interval)
+iv_starts  =[0  RPEAK_ts(valid_idx([next_invalid true]))];     % start of invalid intervals: Timestamps of valid Rpeaks followed by invalid ones
+% First Segment (for 0 to first valid Rpeak) and last segment
+% (everything after last valid Rpeak) are always invalid
+iv_ends    =[RPEAK_ts(valid_idx([true next_invalid]))   inf];  % end of invalid intervals: Timestamps of valid Rpeaks PRECEDED by invalid ones
+% we can get Rpeak-ts preceded by invalid R2R by shifting next_invalid
+grace_window=mean(R2R_valid)/2;                         % +/- Range for shuffled Rpeaks to be allowed inside invalid segments
+
+%% take data corresponding to consecutive R-peaks
+RPEAK_ts     = RPEAK_ts(valid_idx);                                  % take only Rpeaks surrounded by valid R2R
+RPEAK_dur    = R2R_valid(idx_valid_R2R_consec-1);      %
+
+%% remove jittered Rpeaks and corresponding durations that fell into invalid segments
+for iv=1:numel(iv_starts)
+    idx2exclude_1 = RPEAK_ts_p>iv_starts(iv)+grace_window & RPEAK_ts_p<iv_ends(iv)-grace_window;
+    RPEAK_ts_p   (idx2exclude_1) = 0;
+    RPEAK_dur_p (idx2exclude_1) = NaN;
+end
+idx2exclude_2 = RPEAK_ts_p>max(RPEAK_ts)+allowed_jitter_range;
+RPEAK_ts_p    (idx2exclude_2) = 0;
+RPEAK_dur_p  (idx2exclude_2) = NaN;
+
+RPEAK_ts_p   (:,all(RPEAK_ts_p==0,1)) = [];
+RPEAK_dur_p (:,all(isnan(RPEAK_dur_p),1)) = [];
 
 end
