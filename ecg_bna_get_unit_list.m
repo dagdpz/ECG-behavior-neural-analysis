305e0a3a18bbc918b24282508505ce3fe54d2568
lubavas <19555010+lubavas@users.noreply.github.com>
2024-03-19 13:30:36 +0100

    add sites to info

--- ecg_bna_get_unit_list.m
+++ ecg_bna_get_unit_list.m
@@ -9,19 +9,26 @@ version_specific_settings=[keys.db_folder cfg.spikes_version filesep 'ph_project
 run(version_specific_settings)
 keys.anova_table_file=[keys.basepath_to_save cfg.spikes_version filesep 'tuning_table_combined_CI.mat'];
 keys.tuning_table = ph_load_tuning_table(keys); %% load tuning table
-%keys.tuning_table(2:end,3) = cellfun(@(x) x(1:end-2), keys.tuning_table(2:end,3), 'UniformOutput', false);
+% find column indices
+unit_column_index   = DAG_find_column_index(keys.tuning_table, 'unit_ID');
+target_column_index = DAG_find_column_index(keys.tuning_table, 'target');
+site_column_index   = DAG_find_column_index(keys.tuning_table, 'site_ID');
+keys.tuning_table(2:end, target_column_index) = cellfun(@(x) x(1:end-2), keys.tuning_table(2:end, target_column_index), 'UniformOutput', false);
 
 % I. Unit list before exclusion - this one doesn't take number of
 % R-peaks into account
 % create unit list before exclusion criteria and save it
-unit_ids = keys.tuning_table(2:end,1);
-targets  = keys.tuning_table(2:end,3);
+unit_ids = keys.tuning_table(2:end, unit_column_index);
+targets  = keys.tuning_table(2:end, target_column_index);
+sites    = keys.tuning_table(2:end, site_column_index);
+
 filename = [cfg.unit_lists filesep 'unitInfo_before_exclusion'];
-save(filename, 'unit_ids', 'targets')
+save(filename, 'unit_ids', 'targets', 'sites')
 
 % keep unit list before exclusion for later
 unit_ids_before_exclusion = unit_ids;
 targets_before_exclusion  = targets;
+sites_before_exclusion    = sites;
 
 % create table of unit numbers by area
 [unique_areas, ~, ic] = unique(targets);
@@ -38,10 +45,11 @@ if compute_unit_subsets
     keys.tt.n_spikes=keys.cal.n_spikes;
     keys.monkey=''; %% empty because we ignore which monkey it is basically
     keys=ph_tuning_table_correction(keys);
-    unit_ids = keys.tuning_table(2:end,1);
-    targets  = keys.tuning_table(2:end,3);
+    unit_ids = keys.tuning_table(2:end, unit_column_index);
+    targets  = keys.tuning_table(2:end, target_column_index);
+    sites    = keys.tuning_table(2:end, site_column_index);
     filename = [cfg.unit_lists filesep 'unitInfo_after_exclusion_stableTaskAndRest'];
-    save(filename, 'unit_ids', 'targets')
+    save(filename, 'unit_ids', 'targets', 'sites')
     % 2. create table of unit numbers by area
     [unique_areas, ~, ic] = unique(targets);
     unit_counts = accumarray(ic,1);
@@ -52,10 +60,12 @@ if compute_unit_subsets
     ids_excluded = ~ismember(unit_ids_before_exclusion, unit_ids);
     unit_ids_excluded = unit_ids_before_exclusion(ids_excluded);
     targets_excluded  = targets_before_exclusion(ids_excluded);
+    sites_excluded    = sites_before_exclusion(ids_excluded);
     filename = [cfg.unit_lists filesep 'unitInfo_excluded_stableTaskAndRest'];
     unit_ids = unit_ids_excluded;
     targets  = targets_excluded;
-    save(filename, 'unit_ids', 'targets')
+    sites    = sites_excluded;
+    save(filename, 'unit_ids', 'targets', 'sites')
     [unique_areas, ~, ic] = unique(targets_excluded);
     unit_counts = accumarray(ic,1);
     T = table(unique_areas, unit_counts);
@@ -68,18 +78,20 @@ if compute_unit_subsets
     % 1. choose units that had a task block
     keys.tt.tasktypes = {'Fsac_opt'}; % fixation saccade - for rest
     keys.tuning_table=ph_load_tuning_table(keys); %% load tuning table
-    keys.tuning_table(2:end,3) = cellfun(@(x) x(1:end-2), keys.tuning_table(2:end,3), 'UniformOutput', false);
+    keys.tuning_table(2:end, target_column_index) = cellfun(@(x) x(1:end-2), keys.tuning_table(2:end, target_column_index), 'UniformOutput', false);
     keys = ph_tuning_table_correction(keys);
-    unit_ids_after_exclusion_rest = keys.tuning_table(2:end,1);
-    targets_after_exclusion_rest  = keys.tuning_table(2:end,3);
+    unit_ids_after_exclusion_rest = keys.tuning_table(2:end, unit_column_index);
+    targets_after_exclusion_rest  = keys.tuning_table(2:end, target_column_index);
+    sites_after_exclusion_rest    = keys.tuning_table(2:end, site_column_index);
     
     % 2. choose units that had a rest block
     keys.tt.tasktypes= {'Vsac_opt'}; % fixation saccade - for rest
     keys.tuning_table=ph_load_tuning_table(keys); %% load tuning table
-    keys.tuning_table(2:end,3) = cellfun(@(x) x(1:end-2), keys.tuning_table(2:end,3), 'UniformOutput', false);
+    keys.tuning_table(2:end, target_column_index) = cellfun(@(x) x(1:end-2), keys.tuning_table(2:end, target_column_index), 'UniformOutput', false);
     keys = ph_tuning_table_correction(keys);
-    unit_ids_after_exclusion_task = keys.tuning_table(2:end,1);
-    targets_after_exclusion_task  = keys.tuning_table(2:end,3);
+    unit_ids_after_exclusion_task = keys.tuning_table(2:end, unit_column_index);
+    targets_after_exclusion_task  = keys.tuning_table(2:end, target_column_index);
+    sites_after_exclusion_task    = keys.tuning_table(2:end, site_column_index);
     
     % 3. find units that had at least 1 rest block or 1 task block, figure out
     % targets for those
@@ -89,11 +101,15 @@ if compute_unit_subsets
     targets_after_exclusion = cell(length(unit_ids_after_exclusion), 1);
     targets_after_exclusion(ids_rest) = targets_after_exclusion_rest;
     targets_after_exclusion(ids_task) = targets_after_exclusion_task;
+    sites_after_exclusion   = cell(length(unit_ids_after_exclusion), 1);
+    sites_after_exclusion(ids_rest) = sites_after_exclusion_rest;
+    sites_after_exclusion(ids_task) = sites_after_exclusion_task;
     
     filename = [cfg.unit_lists filesep 'unitInfo_after_exclusion'];
     unit_ids = unit_ids_after_exclusion;
     targets  = targets_after_exclusion;
-    save(filename, 'unit_ids', 'targets')
+    sites    = sites_after_exclusion;
+    save(filename, 'unit_ids', 'targets', 'sites')
     % clear unit_ids_after_exclusion targets_after_exclusion
     [unique_areas, ~, ic] = unique(targets_after_exclusion);
     unit_counts = accumarray(ic,1);
@@ -105,10 +121,12 @@ if compute_unit_subsets
     ids_excluded = ~ismember(unit_ids, unit_ids_after_exclusion);
     unit_ids_excluded = unit_ids(ids_excluded);
     targets_excluded  = targets(ids_excluded);
+    sites_excluded    = sites(ids_excluded);
     unit_ids = unit_ids_excluded;
     targets  = targets_excluded;
+    sites    = sites_excluded;
     filename = [cfg.unit_lists filesep 'unitInfo_excluded'];
-    save(filename, 'unit_ids', 'targets')
+    save(filename, 'unit_ids', 'targets', 'sites')
     [unique_areas, ~, ic] = unique(targets_excluded);
     unit_counts = accumarray(ic,1);
     T = table(unique_areas, unit_counts);
