26de2982a871d92d04ac926b7d4a4ae72eea77a6
lubavas <19555010+lubavas@users.noreply.github.com>
2024-04-04 19:46:16 +0200

    Update ecg_bna_load_variables.m

--- ecg_bna_load_variables.m
+++ ecg_bna_load_variables.m
@@ -1,5 +1,7 @@
 function dt = ecg_bna_load_variables(cfg,unit_list, folder, data_struct, var_names)
 
+cond_list = {cfg.condition.name};
+
 for varNum = 1:length(var_names)
     
     if strfind(var_names{varNum}, '.')
@@ -11,6 +13,14 @@ for varNum = 1:length(var_names)
             L = cfg.condition(condNum).name;
             
             dt.(L).(beforeDot).(afterDot) = nan(length(unit_list),1);
+        end
+        
+    elseif strcmp(var_names{varNum}, 'pearson_r') | strfind(var_names{varNum}, 'pearson_p')
+        
+        for condNum = 1:length(cfg.condition)
+            L = cfg.condition(condNum).name;
+            
+            dt.(L).(var_names{varNum}) = nan(7,length(unit_list));
         end
         
     else
@@ -36,6 +46,27 @@ for fileNum = 1:length(unit_list)
         disp(['Reading file ' num2str(fileNum) ' out of ' num2str(length(unit_list))])
         A = load([folder filesep currFileInfo.name], data_struct);
         
+        % drop all condition-related fields
+        B = rmfield(A.(data_struct), cond_list);
+        
+        % move non-condition-related fields into the main data structure
+        for fn = fieldnames(B)'
+            if fileNum == 1
+                if ischar(B.(fn{1}))
+                    dt.(fn{1}) = {B.(fn{1})};
+                else
+                    dt.(fn{1}) = B.(fn{1});
+                end
+            else
+                if ischar(B.(fn{1})) || isstruct(B.(fn{1}))
+                    dt.(fn{1}) = {dt.(fn{1}); B.(fn{1})};
+                else
+                    dt.(fn{1}) = [dt.(fn{1}); B.(fn{1})];
+                end
+            end
+            
+        end
+        
         for varNum = 1:length(var_names)
             for condNum = 1:length(cfg.condition)
                 L = cfg.condition(condNum).name;
@@ -60,10 +91,11 @@ for fileNum = 1:length(unit_list)
                     dt.(L).(var_names{varNum})(fileNum, 1:5) = A.(data_struct).(L).(var_names{varNum});
                 elseif strcmp(var_names{varNum}, 'cc_PSTH_feature') || strcmp(var_names{varNum}, 'pp_PSTH_feature')
                     dt.(L).(var_names{varNum})(fileNum) = A.(data_struct).(L).(var_names{varNum})(1);
+                elseif strcmp(var_names{varNum}, 'NrEvents')
+                    dt.(L).(var_names{varNum})(fileNum) = A.(data_struct).(L).(var_names{varNum})';
                 else
-                    
                     if ~isempty(A.(data_struct).(L).(var_names{varNum}))
-                        dt.(L).(var_names{varNum})(fileNum,:) = A.(data_struct).(L).(var_names{varNum});
+                        dt.(L).(var_names{varNum})(fileNum,:) = A.(data_struct).(L).(var_names{varNum})';
                     end
                 end
             end
