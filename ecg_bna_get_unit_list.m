1da3f3107869692ad7b4c112261d37fe9a33bf7f
lubavas <19555010+lubavas@users.noreply.github.com>
2024-05-27 16:25:11 +0200

    Update ecg_bna_get_unit_list.m

--- ecg_bna_get_unit_list.m
+++ ecg_bna_get_unit_list.m
@@ -1,6 +1,7 @@
 function keys=ecg_bna_get_unit_list(cfg,compute_unit_subsets)
+% apply exclusion criteria, save lists of included and excluded units
+% requires the processed part of cardioballistic analysis
 
-%% apply exclusion criteria, save lists of included and excluded units
 keys=struct;
 keys=ph_general_settings(cfg.project,keys);
 project_specific_settings=[keys.db_folder 'ph_project_settings.m'];
@@ -31,30 +32,152 @@ targets_before_exclusion  = targets;
 sites_before_exclusion    = sites;
 
 % create table of unit numbers by area
-[unique_areas, ~, ic] = unique(targets);
-unit_counts = accumarray(ic,1);
-T = table(unique_areas, unit_counts);
-writetable(T, [filename '.xls'])
-%     clear T unique_areas ic unit_counts filename
+write_unit_table(targets, filename);
+clear unit_ids targets sites
 
 if compute_unit_subsets
     % II. Unit list for BOTH task and rest
-    % 1. apply exclusion criteria - choose only cells for BOTH task and rest
-    keys.tt.tasktypes= {'Fsac_opt','Vsac_opt'};
-    keys.tt.FR=keys.cal.FR;
-    keys.tt.n_spikes=keys.cal.n_spikes;
-    keys.monkey=''; %% empty because we ignore which monkey it is basically
-    keys=ph_tuning_table_correction(keys);
+    % 0. Set up exclusion crtieria
+    SNR       = keys.tt.avg_SNR;
+    stability = keys.tt.avg_stability;
+    FR        = keys.cal.FR;
+    
+    keys.tt.avg_stability = [-Inf Inf]; % drop stability thresholds, we'll implement it from 'criteria' in the neuronal data
+    keys.tt.avg_SNR       = [-Inf Inf]; % drop SNR thresholds, we'll implement it from 'criteria' in the neuronal data
+    keys.tt.tasktypes     = {''};       % drop conditions here, we'll implement exclusion by those in the next step
+    keys.tt.FR            = [0 Inf];% drop FR, we'll use it on the next step
+    keys.monkey           = ''; %% empty because we ignore which monkey it is basically
+    % 1. Start with units with at least 1 spike (non-empty clusters)
+    keys.tt.n_spikes      = keys.cal.n_spikes; % add 
+    keys                  = ph_tuning_table_correction(keys);
+    % 1.1. create unit list
     unit_ids = keys.tuning_table(2:end, unit_column_index);
     targets  = keys.tuning_table(2:end, target_column_index);
     sites    = keys.tuning_table(2:end, site_column_index);
-    filename = [cfg.unit_lists filesep 'unitInfo_after_exclusion_stableTaskAndRest'];
+    filename = [cfg.unit_lists filesep 'unitInfo_after_spike_exclusion_stable'];
     save(filename, 'unit_ids', 'targets', 'sites')
-    % 2. create table of unit numbers by area
-    [unique_areas, ~, ic] = unique(targets);
-    unit_counts = accumarray(ic,1);
-    T = table(unique_areas, unit_counts);
-    writetable(T, [filename '.xls'])
+    % 1.2. [passed] create and save unit count table
+    write_unit_table(targets, filename);
+    % 1.3. figure out units excluded by number of spikes and save those
+    unit_ids_after_spike_exclusion_stable = unit_ids;
+    targets_after_spike_exclusion_stable  = targets;
+    sites_after_spike_exclusion_stable    = sites;
+    % find excluded units
+    [unit_ids, ia] = setdiff(unit_ids_before_exclusion, unit_ids_after_spike_exclusion_stable); % 1 - before current exclusion step, 2 - after
+    targets  = targets_before_exclusion(ia);
+    sites    = sites_before_exclusion(ia);
+    filename = [cfg.unit_lists filesep 'unitInfo_excluded_by_spike_stable'];
+    save(filename, 'unit_ids', 'targets', 'sites')
+    % 1.4. [excluded] create and save unit count table
+    write_unit_table(targets, filename);
+    clear unit_ids targets sites
+    
+    % 2. Exclude units not having both conditions
+    keys.tt.tasktypes     = {'Fsac_opt','Vsac_opt'}; % WE NEED BOTH CONDITIONS HERE
+    keys                  = ph_tuning_table_correction(keys);
+    % 2.1. create unit list
+    unit_ids = keys.tuning_table(2:end, unit_column_index);
+    targets  = keys.tuning_table(2:end, target_column_index);
+    sites    = keys.tuning_table(2:end, site_column_index);
+    filename = [cfg.unit_lists filesep 'unitInfo_after_condition_exclusion_stable'];
+    save(filename, 'unit_ids', 'targets', 'sites')
+    % 2.2. [passed] create and save unit count table
+    write_unit_table(targets, filename);
+    % 2.3. figure out units excluded by number of spikes and save those
+    unit_ids_after_condition_exclusion_stable = unit_ids;
+%     targets_after_condition_exclusion_stable  = targets;
+%     sites_after_condition_exclusion_stable    = sites;
+    % find excluded units
+    [unit_ids, ia] = setdiff(unit_ids_after_spike_exclusion_stable, unit_ids_after_condition_exclusion_stable); % 1 - before current exclusion step, 2 - after
+    targets  = targets_after_spike_exclusion_stable(ia);
+    sites    = sites_after_spike_exclusion_stable(ia);
+    filename = [cfg.unit_lists filesep 'unitInfo_excluded_by_condition_stable'];
+    save(filename, 'unit_ids', 'targets', 'sites')
+    % 2.4. [excluded] create and save unit count table
+    write_unit_table(targets, filename);
+    clear unit_ids targets sites
+    
+    % 3. Exclude by FR
+    keys.tt.FR            = FR;
+    keys                  = ph_tuning_table_correction(keys);
+    % 3.1. create unit list
+    unit_ids = keys.tuning_table(2:end, unit_column_index);
+    targets  = keys.tuning_table(2:end, target_column_index);
+    sites    = keys.tuning_table(2:end, site_column_index);
+    filename = [cfg.unit_lists filesep 'unitInfo_after_FR_exclusion_stable'];
+    save(filename, 'unit_ids', 'targets', 'sites')
+    % 3.2. [passed] create and save unit count table
+    write_unit_table(targets, filename);
+    % 3.3. figure out units excluded by FR and save those
+    unit_ids_after_FR_exclusion_stable = unit_ids;
+    targets_after_FR_exclusion_stable  = targets;
+    sites_after_FR_exclusion_stable    = sites;
+    % find excluded units
+    [unit_ids, ia] = setdiff(unit_ids_after_spike_exclusion_stable, unit_ids_after_FR_exclusion_stable);
+    targets  = targets_before_exclusion(ia);
+    sites    = sites_before_exclusion(ia);
+    filename = [cfg.unit_lists filesep 'unitInfo_excluded_by_FR_stable'];
+    save(filename, 'unit_ids', 'targets', 'sites')
+    % 3.4. [excluded] create and save unit count table
+    write_unit_table(targets, filename);
+    clear unit_ids targets sites
+    
+    % 4. Exclude by SNR
+    % 4.0. load 'criteria' structure for units that passed FR cirterion
+    curr_unit_ids = unit_ids_after_FR_exclusion_stable;
+    dt = ecg_bna_load_variables(cfg, curr_unit_ids, 'cardioballistic', 'data', {});
+    % 4.1. [passed] exclude by SNR
+    task_SNR_ids  = dt.criteria.SNR_F >= SNR(1) & dt.criteria.SNR_F <= SNR(2);
+    rest_SNR_ids  = dt.criteria.SNR_V >= SNR(1) & dt.criteria.SNR_V <= SNR(2);
+    SNR_ids       = task_SNR_ids & rest_SNR_ids;
+    unit_ids      = unit_ids_after_FR_exclusion_stable(SNR_ids);
+    targets       = targets_after_FR_exclusion_stable(SNR_ids);
+    sites         = sites_after_FR_exclusion_stable(SNR_ids);
+    filename = [cfg.unit_lists filesep 'unitInfo_after_SNR_exclusion_stable'];
+    save(filename, 'unit_ids', 'targets', 'sites')
+    % 4.2. [passed] save table
+    write_unit_table(targets, filename);
+    % 4.3. figure out units excluded by SNR and save those
+    unit_ids_after_SNR_exclusion_stable = unit_ids;
+    targets_after_SNR_exclusion_stable  = targets;
+    sites_after_SNR_exclusion_stable    = sites;
+    
+    % find excluded units
+    [unit_ids, ia] = setdiff(unit_ids_after_FR_exclusion_stable, unit_ids_after_SNR_exclusion_stable);
+    targets  = targets_after_FR_exclusion_stable(ia);
+    sites    = sites_after_FR_exclusion_stable(ia);
+    filename = [cfg.unit_lists filesep 'unitInfo_excluded_by_SNR_stable'];
+    save(filename, 'unit_ids', 'targets', 'sites')
+    % 4.4. [excluded] create and save unit count table
+    write_unit_table(targets, filename);
+    clear unit_ids targets sites
+    
+    % 5. Exclude by stability
+    % 5.0. load 'criteria' structure for units that passed FR crIterion
+%     curr_unit_ids = unit_ids_after_SNR_exclusion_stable;
+%     dt = ecg_bna_load_variables(cfg, curr_unit_ids, 'cardioballistic', 'data', {});
+    % 5.1. [passed] exclude by stability
+    task_stab_ids = dt.criteria.stability_F >= stability(1) & dt.criteria.stability_F <= stability(2);
+    rest_stab_ids = dt.criteria.stability_V >= stability(1) & dt.criteria.stability_V <= stability(2);
+    stability_ids = task_stab_ids & rest_stab_ids;
+    unit_ids      = unit_ids_after_FR_exclusion_stable(SNR_ids & stability_ids); % take list after FR exclusion and excluded both SNR and stability to load criteria only once
+    targets       = targets_after_FR_exclusion_stable(SNR_ids & stability_ids);
+    sites         = sites_after_FR_exclusion_stable(SNR_ids & stability_ids);
+    filename = [cfg.unit_lists filesep 'unitInfo_after_stability_exclusion_stable'];
+    save(filename, 'unit_ids', 'targets', 'sites')
+    % 5.2. write_unit_table(targets, filename);
+    write_unit_table(targets, filename);
+    % 5.3. figure out units excluded by stability and save those
+    unit_ids_after_stability_exclusion_stable = unit_ids;
+    % find excluded units
+    [unit_ids, ia] = setdiff(unit_ids_after_SNR_exclusion_stable, unit_ids_after_stability_exclusion_stable);
+    targets  = targets_after_SNR_exclusion_stable(ia);
+    sites    = sites_after_SNR_exclusion_stable(ia);
+    filename = [cfg.unit_lists filesep 'unitInfo_excluded_by_stability_stable'];
+    save(filename, 'unit_ids', 'targets', 'sites')
+    % 5.4. [excluded] create and save unit count table
+    write_unit_table(targets, filename);
+    clear unit_ids targets sites
     
     % III. Unit list of excluded units (that aren't units existing for both task and rest)
     ids_excluded = ~ismember(unit_ids_before_exclusion, unit_ids);
@@ -134,4 +257,11 @@ if compute_unit_subsets
     %     clear T unique_areas ic unit_counts filename unit_ids_excluded targets_excluded unit_ids_excluded targets_excluded unit_ids_before_exclusion targets_before_exclusion
 end
 
+end
+
+function write_unit_table(targets, filename)
+[unique_areas, ~, ic] = unique(targets);
+unit_counts = accumarray(ic,1);
+T = table(unique_areas, unit_counts);
+writetable(T, [filename '.xls'])
 end
