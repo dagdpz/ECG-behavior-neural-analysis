678b8591817f42cbef6ba949e9e37c99d6f297c6
lubavas <19555010+lubavas@users.noreply.github.com>
2024-07-09 13:19:00 +0200

    add scripts for correlation analysis

--- ecg_bna_compute_session_correlation_analysis.m
+++ ecg_bna_compute_session_correlation_analysis.m
@@ -48,24 +48,28 @@ for unitNum = 1:length(population)
     data.stability_rating  = pop.avg_stability;
     data.thresholds_microV = single([NaN; NaN; NaN; NaN]);
     data.FR                = single(mean(pop.FR_average));
-    data.cc_lag_list       = cfg.spk.lag_list;
+    data.cc_lag_list       = cfg.correlation.lag_list;
     data.criteria          = pop.criteria;
     for c=1:numel(cfg.condition)
         L=cfg.condition(c).name;
-        data.(L).meanHR_bpm                     = single(NaN);
-        data.(L).medianHR_bpm                   = single(NaN);
-        data.(L).stdHR_bpm                      = single(NaN);
-        data.(L).SDNN_ms                        = single(NaN);
-        data.(L).cc_PSTH_feature                = single(nan(1,4));
-        data.(L).pp_PSTH_feature                = single(nan(1,4));
-        data.(L).pperm_PSTH_feature             = single(nan(1,4)); % permuted p for the correlation between each feature and phase PSTH
-        data.(L).timeRRstart                    = single(nan(1,1));
-        data.(L).FRbyRR_Hz                      = single(nan(1,1));
-        data.(L).cycleDurations_s               = single(nan(1,1));
-        data.(L).n_cycles                       = single(nan(1,1));
-        data.(L).pearson_r                      = single(nan(length(cfg.spk.lag_list), 1));
-        data.(L).pearson_p                      = single(nan(length(cfg.spk.lag_list), 1));
-        data.(L).permuted_p                     = single(nan(length(cfg.spk.lag_list), 1));
+        data.(L).valid_RRinterval_starts = single(nan(1,1));
+        data.(L).valid_RRinterval_ends   = single(nan(1,1));
+        data.(L).AT_one_stream           = single(nan(1,1));
+        data.(L).timeRRstart             = single(nan(1,1));
+        data.(L).FRbyRR_Hz               = single(nan(1,1));
+        data.(L).cycleDurations_s        = single(nan(1,1));
+        data.(L).n_cycles                = single(nan(1,1));
+        data.(L).pearson_r               = single(nan(length(cfg.correlation.lag_list), 1));
+        data.(L).pearson_p               = single(nan(length(cfg.correlation.lag_list), 1));
+        data.(L).permuted_p              = single(nan(length(cfg.correlation.lag_list), 1));
+        data.(L).FR_n_cycles             = single(nan(1,1));
+        data.(L).FR_pearson_r            = single(nan(length(cfg.correlation.lag_list), 1));
+        data.(L).FR_pearson_p            = single(nan(length(cfg.correlation.lag_list), 1));
+        data.(L).FR_permuted_p           = single(nan(length(cfg.correlation.lag_list), 1));
+        data.(L).RR_n_cycles             = single(nan(1,1));
+        data.(L).RR_pearson_r            = single(nan(length(cfg.correlation.lag_list), 1));
+        data.(L).RR_pearson_p            = single(nan(length(cfg.correlation.lag_list), 1));
+        data.(L).RR_permuted_p           = single(nan(length(cfg.correlation.lag_list), 1));
     end
     
     % find the corresponding WC file and load it
@@ -104,25 +108,62 @@ for unitNum = 1:length(population)
         trcell=T(tr);
         
         % 0. Prepare data variables
+        % 0.1. Figure out house-keeping variables
         states_onset               = {trcell.states_onset};
         states                     = {trcell.states};
         TDT_ECG1_t0_from_rec_start = {trcell.TDT_ECG1_t0_from_rec_start};
         block_nums                 = {trcell.block};
         state2_times               = cellfun(@(x,y) x(y == 2), states_onset, states, 'Uniformoutput', false); % trial starts = state 2
         state90_times              = cellfun(@(x,y) x(y == 90), states_onset, states, 'Uniformoutput', false); % trial ends = state 90
-        % compute RR-intervals
+%         state98_times              = cellfun(@(x,y) x(y == 98), states_onset, states, 'Uniformoutput', false); % trial ends = state 98
+        % 0.2. Compute RR-intervals
         valid_RRinterval_ends      = single([Rpeaks.(['RPEAK_ts' cfg.condition(c).Rpeak_field])]);
         valid_RRinterval_starts    = single(valid_RRinterval_ends - [Rpeaks.(['RPEAK_dur' cfg.condition(c).Rpeak_field])]);
-        % 0. figure out RR-intervals lying within trials
+        % 1. Figure out RR-intervals lying within trials
         trial_starts_one_stream    = cellfun(@(x,y,z) x+y+Rpeaks([Rpeaks.block] == z).offset, state2_times, TDT_ECG1_t0_from_rec_start, block_nums);
-        trial_ends_one_stream      = cellfun(@(x,y,z) x+y+Rpeaks([Rpeaks.block] == z).offset, state90_times, TDT_ECG1_t0_from_rec_start, block_nums);
+        trial_ends90_one_stream      = cellfun(@(x,y,z) x+y+Rpeaks([Rpeaks.block] == z).offset, state90_times, TDT_ECG1_t0_from_rec_start, block_nums);
+        %         trial_ends98_one_stream      = cellfun(@(x,y,z) x+y+Rpeaks([Rpeaks.block] == z).offset, state98_times, TDT_ECG1_t0_from_rec_start, block_nums);
         
-        RR_within_trial_idx = false(length(valid_RRinterval_starts),1);
-        for RRnum = 1:length(RR_within_trial_idx)
-            RR_within_trial_idx(RRnum) = ...
+        RR_within_trial90_idx = false(length(valid_RRinterval_starts),1);
+        %         RR_within_trial98_idx = false(length(valid_RRinterval_starts),1);
+        for RRnum = 1:length(RR_within_trial90_idx)
+            % indexing for FR variable
+            RR_within_trial90_idx(RRnum) = ...
                 any(valid_RRinterval_starts(RRnum)>trial_starts_one_stream & ...
-                valid_RRinterval_ends(RRnum)<trial_ends_one_stream);
+                valid_RRinterval_ends(RRnum)<trial_ends90_one_stream);
+            
+            %             % indexing for RR variable
+            %             RR_within_trial98_idx(RRnum) = ...
+            %                 any(valid_RRinterval_starts(RRnum)>trial_starts_one_stream & ...
+            %                 valid_RRinterval_ends(RRnum)<trial_ends98_one_stream);
         end
+              
+        % 2. Prepare spiking data
+        % 2.1. take arrival times and the corresponding waveforms
+        AT = {popcell.arrival_times};
+        WF = {popcell.waveforms};
+        % 2.2. choose only those that happen after MP-state 1
+        idx_after_state1 = cellfun(@(x,y) x>y, AT, state2_times, 'Uniformoutput', false);
+        % 2.3. add TDT_ECG1_t0_from_rec_start and Rpeak block offset to spike times
+        AT_one_stream_cell = cellfun(@(x,y,z,a) x(y)+z+Rpeaks([Rpeaks.block] == a).offset, AT, idx_after_state1, TDT_ECG1_t0_from_rec_start, block_nums, 'Uniformoutput', false);
+        WF_one_stream_cell = cellfun(@(x,y) x(y,:), WF, idx_after_state1, 'Uniformoutput', false);
+        % 2.4. merge all spike times and waveforms together
+        AT_one_stream = cat(1, AT_one_stream_cell{:});
+        WF_one_stream = cat(1, WF_one_stream_cell{:});
+        if size(WF_one_stream,1) < 10
+            continue
+        end
+        
+        % check the number of spikes left after computing phases
+        if length(valid_RRinterval_starts) < 3
+            continue
+        end
+        
+        % put data in the output structure - this is to create rasters as
+        % in Kim et al., 2019
+        data.(L).valid_RRinterval_starts = valid_RRinterval_starts(RR_within_trial90_idx);
+        data.(L).valid_RRinterval_ends   = valid_RRinterval_ends(RR_within_trial90_idx);
+        data.(L).AT_one_stream           = AT_one_stream;
         
         % % figure out how many R peaks I need to cover long intervals between R-peaks
         % find long intervals between R-peaks
@@ -142,62 +183,82 @@ for unitNum = 1:length(population)
             valid_RRinterval_ends = [valid_RRinterval_ends(1:ids(intRRnum)) nan(1, n_add_cycles(intRRnum)) valid_RRinterval_ends(ids(intRRnum)+1:end)];
         end
         
-        % 1. take arrival times and the corresponding waveforms
-        AT = {popcell.arrival_times};
-        WF = {popcell.waveforms};
-        % 2. choose only those that happen after MP-state 1
-        idx_after_state1 = cellfun(@(x,y) x>y, AT, state2_times, 'Uniformoutput', false);
-        % 3. add TDT_ECG1_t0_from_rec_start and Rpeak block offset to spike times
-        AT_one_stream_cell = cellfun(@(x,y,z,a) x(y)+z+Rpeaks([Rpeaks.block] == a).offset, AT, idx_after_state1, TDT_ECG1_t0_from_rec_start, block_nums, 'Uniformoutput', false);
-        WF_one_stream_cell = cellfun(@(x,y) x(y,:), WF, idx_after_state1, 'Uniformoutput', false);
-        % 4. merge all spike times and waveforms together
-        AT_one_stream = cat(1, AT_one_stream_cell{:});
-        WF_one_stream = cat(1, WF_one_stream_cell{:});
-        if size(WF_one_stream,1) < 10
-            continue
-        end
         
-        % check the number of spikes left after computing phases
-        if length(valid_RRinterval_starts) < 3
-            continue
-        end
         
         % II. Compute unit firing rate per RR-interval
         data.(L).timeRRstart           = valid_RRinterval_starts;
         [FRbyRR_Hz, cycleDurations_s] = ...
             computeFRperCycle(valid_RRinterval_starts, valid_RRinterval_ends, AT_one_stream);
-        FRbyRR_Hz(~RR_within_trial_idx)        = NaN;
-        cycleDurations_s(~RR_within_trial_idx) = NaN;
+        FRbyRR_Hz(~RR_within_trial90_idx)        = NaN;
+        % option 2
+%         FRbyRR_Hz(~RR_within_trial90_idx)        = NaN;
+%         cycleDurations_s(~RR_within_trial98_idx) = NaN;
+        % option 1
+%         FRbyRR_Hz(~RR_within_trial90_idx)        = NaN;
+%         cycleDurations_s(~RR_within_trial90_idx) = NaN;
         data.(L).FRbyRR_Hz = FRbyRR_Hz;
         data.(L).cycleDurations_s = cycleDurations_s;
         % compute correlation with different lag
-        for lagNum = 1:length(cfg.spk.lag_list)
-            curr_lag = cfg.spk.lag_list(lagNum);
-            % create data variables
-            if curr_lag < 0
-                % If lag is negative, array1 is shifted to the right
-                fr_hz = FRbyRR_Hz(1:end+curr_lag);
-                rr_s  = cycleDurations_s(1-curr_lag:end);
-            else
-                % If lag is positive or zero, array2 is shifted to the right
-                fr_hz = FRbyRR_Hz(1+curr_lag:end);
-                rr_s  = cycleDurations_s(1:end-curr_lag);
-            end
-            
-            % compute correlation coefficient
-            [temp_r, temp_p] = corrcoef(fr_hz, rr_s, 'Rows', 'pairwise');
-            data.(L).n_cycles = sum(~isnan(fr_hz) & ~isnan(rr_s));
-            data.(L).pearson_r(lagNum) = temp_r(2,1);
-            data.(L).pearson_p(lagNum) = temp_p(2,1);
-            data.(L).permuted_p(lagNum) = mult_comp_perm_corr(fr_hz, rr_s, cfg.spk.n_permutations, 0, 0.05, 'linear', 0);
-            clear temp_r temp_p
-%             xcorr
-        end
+        tic
+        [data.(L).n_cycles, data.(L).pearson_r, data.(L).pearson_p, data.(L).permuted_p] = ...
+            compute_correlation_by_lag(cfg,FRbyRR_Hz,cycleDurations_s);
+        toc
+        
+        % autocorrelation FRbyRR_Hz
+        [data.(L).FR_n_cycles, data.(L).FR_pearson_r, data.(L).FR_pearson_p, data.(L).FR_permuted_p] = ...
+            compute_correlation_by_lag(cfg,FRbyRR_Hz,FRbyRR_Hz);
+        
+        % autocorrelation cycleDurations_s
+        [data.(L).RR_n_cycles, data.(L).RR_pearson_r, data.(L).RR_pearson_p, data.(L).RR_permuted_p] = ...
+            compute_correlation_by_lag(cfg,cycleDurations_s,cycleDurations_s);
+        
     end
     save([basepath_to_save filesep data.unitId '_' data.target '_correlation.mat'], 'data', '-v7.3')
     clear data
 
 end
+end
+
+function [n_cycles, pearson_r, pearson_p, permuted_p] = compute_correlation_by_lag(cfg,FRbyRR_Hz,cycleDurations_s)
+n_permutations = cfg.correlation.n_permutations;
+tail           = cfg.correlation.tail;
+alpha_level    = cfg.correlation.alpha_level;
+stat           = cfg.correlation.stat;
+reports        = cfg.correlation.reports;
+seed_state     = cfg.correlation.seed_state;
+
+% Initialize output structure to avoid broadcasting issues
+lag_list        = cfg.correlation.lag_list;
+nLags           = length(cfg.correlation.lag_list);
+n_cycles   = zeros(1, nLags);
+pearson_r  = zeros(1, nLags);
+pearson_p  = zeros(1, nLags);
+permuted_p = zeros(1, nLags);
+
+parfor lagNum = 1:length(cfg.correlation.lag_list)
+    curr_lag = lag_list(lagNum);
+    % create data variables
+    if curr_lag < 0
+        % If lag is negative, rr_s is shifted to the right
+        fr_hz = FRbyRR_Hz(1:end+curr_lag);
+        rr_s  = cycleDurations_s(1-curr_lag:end);
+    else
+        % If lag is positive or zero, fr_hz is shifted to the right
+        fr_hz = FRbyRR_Hz(1+curr_lag:end);
+        rr_s  = cycleDurations_s(1:end-curr_lag);
+    end
+    
+    % compute correlation coefficient
+    [~,fr_hz_nan] = rmmissing(fr_hz);
+    [~,rr_s_nan] = rmmissing(rr_s);
+    [temp_r, temp_p] = corrcoef(fr_hz, rr_s, 'Rows', 'pairwise');
+    n_cycles(lagNum)  = sum(~isnan(fr_hz) & ~isnan(rr_s));
+    pearson_r(lagNum) = temp_r(2,1);
+    pearson_p(lagNum) = temp_p(2,1);
+    permuted_p(lagNum) = mult_comp_perm_corr(fr_hz(~fr_hz_nan & ~rr_s_nan), rr_s(~fr_hz_nan & ~rr_s_nan), ...
+        n_permutations, tail, alpha_level, stat, reports, seed_state);
+end
+
 end
 
 function [FRbyRR_Hz, cycleDurations] = computeFRperCycle(interval_starts, interval_ends, eventTimes)
---
+++ ecg_bna_plot_session_correlation.m
@@ -0,0 +1,218 @@
+function ecg_bna_plot_session_correlation(session_info,cfg)
+
+matlab_year = version('-release');
+matlab_year = str2double(matlab_year(1:end-1));
+
+dataFolder = [cfg.SPK_root_results_fldr filesep 'correlation_analysis' filesep];
+
+condition_colors={cfg.condition.color};
+
+fileList = dir([dataFolder session_info.Monkey(1:3) '_' session_info.Date '*correlation.mat']);
+
+for untNum = 1:length(fileList)
+    
+    load([dataFolder filesep fileList(untNum).name], 'data')
+
+    %% title with all the spike analysis parameters
+    sgtitleText = {[data.unitId '_' data.target '; ch ' num2str(data.channel) ';'  ' unit ' data.unit], ... %  
+        ['FR, Hz: ' num2str(data.FR) '; SNR: ' num2str(data.quantSNR) '; Stability ratio: ' num2str(data.stability_rating) '; % of ISIs < 3 ms: '  num2str(100 * data.Single_rating) '%'], ...
+        ['Rest: Pearson r (lag 0) = ' num2str(data.Rest.pearson_r(4)) ' p = ' num2str(data.Rest.pearson_p(4)) ...
+            '; Task: Pearson r (lag 0) = ' num2str(data.Task.pearson_r(4)) '; p = ' num2str(data.Task.pearson_p(4))]};
+    
+    %% rasters with sorting by RR duration
+    f1 = figure;
+    set(f1, 'Position', [376 64 1140 928])
+    
+    if matlab_year >= 2020
+        sgtitle(sgtitleText, 'interpreter', 'none')
+    end
+    
+    for c=1:numel(cfg.condition)
+        L=cfg.condition(c).name;
+        col=cfg.condition(c).color;
+        
+        % compute raster
+        AT        = data.(L).AT_one_stream;
+        RR_starts = data.(L).valid_RRinterval_starts;
+        RR_ends   = data.(L).valid_RRinterval_ends;
+        
+        % drop RRs before the 1st spike or after the last one
+        drop_RR_ids = RR_starts < AT(1) | RR_ends > AT(end);
+        RR_starts(drop_RR_ids) = [];
+        RR_ends(drop_RR_ids)   = [];
+        
+        RR_starts              = rmmissing(RR_starts);
+        RR_ends                = rmmissing(RR_ends);
+        
+        % select spikes per cardiac cycle
+        raster = zeros(length(RR_starts),801);
+        for cycleNum = 1:length(RR_starts)
+            
+            currCycleSpikeIds  = AT > RR_starts(cycleNum) & AT < RR_ends(cycleNum);
+            raster(cycleNum,:) = histc(AT(currCycleSpikeIds)-RR_starts(cycleNum), 0:0.001:0.8);
+            
+        end
+        
+        % compute scatter input
+        subplot(2,numel(cfg.condition),2*(c-1)+1)
+        [raster_row, raster_col] = find(raster);
+        scatter(raster_col,raster_row,2,'MarkerFaceColor',col,'MarkerFaceAlpha',0.2,'MarkerEdgeColor',col,'MarkerEdgeAlpha',0.2);
+        hold on
+        scatter(1000*(RR_ends-RR_starts),1:length(RR_starts), 'Marker','^')
+        box on
+        xlim([0 550])
+        if ~isempty(raster)
+            ylim([0 size(raster,1)])
+        end
+        xlabel('Time after R-peak, ms')
+        ylabel('R-peak #')
+        title([L ': Original R-peak Order'])
+        
+        subplot(2,numel(cfg.condition),2*c)
+        [RRdur_sorted,ids] = sort(1000*(RR_ends-RR_starts));
+        raster_sorted = raster(ids,:);
+        [raster_row, raster_col] = find(raster_sorted);
+        scatter(raster_col,raster_row,2,'MarkerFaceColor',col,'MarkerFaceAlpha',0.2,'MarkerEdgeColor',col,'MarkerEdgeAlpha',0.2);
+        hold on
+        scatter(RRdur_sorted,1:length(RR_starts), 'Marker','^')
+        box on
+        xlim([0 550])
+        if ~isempty(raster)
+            ylim([0 size(raster,1)])
+        end
+        title([L ': Sorted by RR duration from Low of High'])
+        
+    end
+    
+    filename= ['RastersSorted__' data.unitId, '_' data.target];
+    saveas(f1,[dataFolder, filesep, filename], 'fig')
+    saveas(f1,[dataFolder, filesep, filename], 'tiff')
+    close(f1);
+    
+    
+    %% results of correlation analysis
+    f5 = figure;
+    set(f5, 'Position', [381 424 1450 398])
+    if matlab_year >= 2020
+        sgtitle(sgtitleText, 'interpreter', 'none')
+    end
+    
+    for c=1:numel(cfg.condition)
+        L=cfg.condition(c).name;
+        subplot(1,numel(cfg.condition)+2,c)
+        scatter(data.(L).FRbyRR_Hz, data.(L).cycleDurations_s, [], condition_colors{c}(1:3), 'Marker', '.')
+        hold on
+        linTrend = lsline(gca);
+        linTrend.Color = condition_colors{c}(1:3);
+        xlabel('Firing Rate per Heart Cycle, Hz')
+        ylabel('Heart Cycle Duration, s')
+        box on
+        title([L ': cc = ' num2str(data.(L).pearson_r(4)) '; p = ' num2str(data.(L).permuted_p(4))])
+        legend({'Real Data', 'Least-Square Fit'}, 'Location', 'Best')
+    end
+    
+    subplot(1,numel(cfg.condition)+2,numel(cfg.condition)+2)
+    box on
+    hold on
+    for c=1:numel(cfg.condition)
+        L=cfg.condition(c).name;
+        plot(data.cc_lag_list, data.(L).pearson_r, '-o', 'Color', condition_colors{c}(1:3))
+    end
+    ylim([-0.4 0.4])
+    xlabel('Lag: Number of Heart Cycles Shifted')
+    ylabel('Correlation Coefficient')
+    legend({cfg.condition.name})
+    
+    filename= ['FR_RR_Correlations__' data.unitId, '_' data.target];
+    export_fig(gcf,[dataFolder, filesep, filename], '-pdf','-transparent') % pdf by run
+    close(gcf);
+    
+    %% unit FR per heart cycle as a function of time
+	figure;
+    set(gcf, 'Position', [1 41 1920 482])
+
+    for c=1:numel(cfg.condition)
+        L=cfg.condition(c).name;
+        yyaxis left
+        set(gca,'YColor',cfg.condition(c).color);
+        hold on
+        plot(data.(L).timeRRstart, data.(L).FRbyRR_Hz, 'o-', 'Color', cfg.condition(c).color)
+        ylabel('FR per Heart Cycle, Hz')
+        yyaxis right
+        set(gca,'YColor',[0 0 0]);
+        hold on
+        plot(data.(L).timeRRstart, data.(L).cycleDurations_s, 'kx--')
+        ylabel('Heart-Cycle Duration, s')
+        box on
+        
+        xlim_lows(c)  = data.(L).timeRRstart(1);
+        xlim_highs(c) = data.(L).timeRRstart(end);
+    end
+    xlabel('Time from Session Start, s')
+    if ~isnan(min(xlim_lows)) && ~isnan(max(xlim_highs))
+        xlim([min(xlim_lows) max(xlim_highs)])
+    end
+    if matlab_year >= 2020
+        sgtitle(sgtitleText, 'interpreter', 'none')
+    end
+    
+    filename= ['Time_vs_FRperCycle_' data.unitId, '_' data.target];
+    export_fig(gcf,[dataFolder, filesep, filename], '-pdf','-transparent') % pdf by run
+    close(gcf);
+    
+    %% unit FR by RR-cycle quintiles
+    figure,
+    set(gcf, 'Position', [381 201 1450 795])
+    if matlab_year >= 2020
+        sgtitle(sgtitleText, 'interpreter', 'none')
+    end
+    
+    for c=1:numel(cfg.condition)
+        L=cfg.condition(c).name;
+        
+        RRdur_quintiles = prctile(data.(L).cycleDurations_s, 0:20:100);
+        
+        avgFR_per_quintile = nan(length(RRdur_quintiles)-1,1);
+        SEM_per_quintile   = nan(length(RRdur_quintiles)-1,1);
+        for quintNum = 1:length(RRdur_quintiles)-1
+            curr_RR_ids     = data.(L).cycleDurations_s > RRdur_quintiles(quintNum) & data.(L).cycleDurations_s < RRdur_quintiles(quintNum+1);
+            avgFR_per_quintile(quintNum) = nanmean(data.(L).FRbyRR_Hz(curr_RR_ids));
+            SEM_per_quintile(quintNum) = sterr(data.(L).FRbyRR_Hz(curr_RR_ids));
+        end
+        
+        subplot(2,numel(cfg.condition), c)
+        errorbar(1:5, avgFR_per_quintile, SEM_per_quintile, 'Color', cfg.condition(c).color)
+        xlim([0 6])
+        box on
+        title([cfg.condition(c).name ': Pearson r (lag 0) = ' num2str(data.(L).pearson_r(4)) ' p = ' num2str(data.(L).pearson_p(4))])
+        xlabel('RR duration Quintile Number')
+        ylabel('Firing Rate, Hz')
+        
+        % RR durations sorted in ascending order vs. unit FR
+        subplot(2,numel(cfg.condition), c + 2)
+        [currRRdur, sort_ids] = sort(data.(L).cycleDurations_s);
+        currFR                = data.(L).FRbyRR_Hz(sort_ids);
+        RRbins                = min(data.(L).cycleDurations_s):0.01:max(data.(L).cycleDurations_s);
+        avgFR_per_bin = NaN;
+        for binNum = 1:length(RRbins)-1
+            curr_RR_ids = currRRdur > RRbins(binNum) & currRRdur < RRbins(binNum+1);
+            avgFR_per_bin(binNum) = mean(currFR(curr_RR_ids));
+            semFR_per_bin(binNum) = sterr(currFR(curr_RR_ids));
+        end
+        
+        plot(currRRdur, currFR, 'Color', cfg.condition(c).color/2)
+        hold on
+        if ~isnan(avgFR_per_bin)
+            errorbar(RRbins(1:end-1)+0.005, avgFR_per_bin, semFR_per_bin, 'Color', cfg.condition(c).color, 'Marker', 'o', 'LineWidth', 2)
+        end
+        xlabel('RR durations (s) in ascending order')
+        ylabel('FR in single cardiac cycles, Hz')
+        legend('For Individual Cardiac Cycles', 'Averaged by Bin')
+        clear curr_RR_ids avgFR_per_bin semFR_per_bin
+    end
+    
+    filename= ['RRquintile_vs_FRperCycle_' data.unitId, '_' data.target];
+    export_fig(gcf,[dataFolder, filesep, filename], '-pdf','-transparent') % pdf by run
+    close(gcf);
+    
+end
