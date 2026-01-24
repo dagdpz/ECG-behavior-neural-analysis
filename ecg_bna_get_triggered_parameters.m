b8312e002632454f96a2641ea6d945f01185aa14
luschn <lukas.schneider.m@gmail.com>
2026-01-24 14:34:40 +0100

    workaround for complex mua

--- ecg_bna_get_triggered_parameters.m
+++ ecg_bna_get_triggered_parameters.m
@@ -138,6 +138,7 @@ for s = 1: numel(w_samples)
         end
         
         if n_iterations==1 && ismember(fn,{'evoked'})% && ismember(fni,{'ecg'})
+            AA=real(AA);
             triggered.(fn).mean(1,:,s)        = single(mean(AA,3));
             triggered.(fn).std(1,:,s)         = single(std(AA,0,3));
             triggered.(fn).sterr(1,:,s)       = single(sterr(AA,3));
